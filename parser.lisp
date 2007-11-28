97a21f66c51f499d595b1a59771e8d48dda22d52
John Wiegley <johnw@newartisans.com>
2007-11-28 07:54:40 -0400

    Revised the parser's structure (but it still doesn't work)

--- parser.lisp
+++ parser.lisp
@@ -4,229 +4,266 @@
 
 ;;;_ * Simple numerics
 
+(defvar *next-token* nil)
+
+(defun peek-token (in &optional (eof-error-p t))
+  (or *next-token*
+      (progn
+	(peek-char t in eof-error-p)	; skip whitespace
+	(setf *next-token* (read in eof-error-p)))))
+
+(defun read-token (in &optional (eof-error-p t))
+  (if *next-token*
+      (prog1
+	  *next-token*
+	(setf *next-token* nil))
+      (progn
+	(peek-char t in eof-error-p)	; skip whitespace
+	(read in eof-error-p))))
+
+(defun unread-token (in token)
+  (declare (ignore in))
+  (assert (null *next-token*))
+  (setf *next-token* token))
+
 ;;;_  + Coded
 
-(defprod p/ws () (+ (/ #\Space #\Tab #\Newline)))
-
-(defchartype p/digit '(satisfies digit-char-p))
-
-(defprod p/digits () (+ p/digit))
-
-(defprod p/number ()
-  (^ p/digits (parse-integer p/digits)))
+(defun p/number (in)
+  (if (digit-char-p (peek-char t in))
+      (read in)))
 
 ;;;_  + English units
 
-(defprod p/cardinal ()
-  (/
-   (^ p/number p/number)
-   (^ "one" 1)
-   (^ "two" 2)
-   (^ "three" 3)
-   (^ "four" 4)
-   (^ "five" 5)
-   (^ "six" 6)
-   (^ "seven" 7)
-   (^ "eight" 8)
-   (^ "nine" 9)
-   (^ "ten" 10)))
+(defun p/cardinal (in)
+  (or (p/number in)
+      (let ((token (peek-token in)))
+	(case token
+	  (one (read-token in) 1)
+	  (two (read-token in) 2)
+	  (three (read-token in) 3)
+	  (four (read-token in) 4)
+	  (five (read-token in) 5)
+	  (six (read-token in) 6)
+	  (seven (read-token in) 7)
+	  (eight (read-token in) 8)
+	  (nine (read-token in) 9)
+	  (ten (read-token in) 10)))))
 
-(defprod p/ordinal ()
-  (/ 
-   (^ (/ "1st" "first") (list :ordinal 1))
-   (^ "other" (list :other))
-   (^ (/ "2nd" "second") (list :ordinal 2))
-   (^ (/ "3rd" "third") (list :ordinal 3))
-   (^ (/ "4th" "fourth") (list :ordinal 4))
-   (^ (/ "5th" "fifth") (list :ordinal 5))
-   (^ (/ "6th" "sixth") (list :ordinal 6))
-   (^ (/ "7th" "seventh") (list :ordinal 7))
-   (^ (/ "8th" "eighth") (list :ordinal 8))
-   (^ (/ "9th" "ninth") (list :ordinal 9))
-   (^ (/ "10th" "tenth") (list :ordinal 10))))
+(defun p/ordinal (in)
+  (let ((token (peek-token in)))
+    ;; jww (2007-11-28): I no longer parse "the other week", or "every other
+    ;; week"
+    (case token
+      ((1st first) (read-token in) (list :ordinal 1))
+      ((2nd second) (read-token in) (list :ordinal 2))
+      ((3rd third) (read-token in) (list :ordinal 3))
+      ((4th fourth) (read-token in) (list :ordinal 4))
+      ((5th fifth) (read-token in) (list :ordinal 5))
+      ((6th sixth) (read-token in) (list :ordinal 6))
+      ((7th seventh) (read-token in) (list :ordinal 7))
+      ((8th eight) (read-token in) (list :ordinal 8))
+      ((9th ninth) (read-token in) (list :ordinal 9))
+      ((10th tenth) (read-token in) (list :ordinal 10)))))
 
-(defprod p/days-of-week ()
-  (/
-   (^ "sunday" (list :day-of-week 0))
-   (^ "monday" (list :day-of-week 1))
-   (^ "tuesday" (list :day-of-week 2))
-   (^ "wednesday" (list :day-of-week 3))
-   (^ "thursday" (list :day-of-week 4))
-   (^ "friday" (list :day-of-week 5))
-   (^ "saturday" (list :day-of-week 6))))
+(defun p/days-of-week (in)
+  (let ((token (peek-token in)))
+    (case token
+      (sunday (read-token in) (list :day-of-week 0))
+      (monday (read-token in) (list :day-of-week 1))
+      (tuesday (read-token in) (list :day-of-week 2))
+      (wednesday (read-token in) (list :day-of-week 3))
+      (thursday (read-token in) (list :day-of-week 4))
+      (friday (read-token in) (list :day-of-week 5))
+      (saturday (read-token in) (list :day-of-week 6)))))
 
-(defprod p/months-of-year ()
-  (/
-   (^ (/ "January" "january" "Jan" "jan") (list :month 1))
-   (^ (/ "February" "january" "Jan" "jan") (list :month 2))
-   (^ (/ "March" "march" "Mar" "mar") (list :month 3))
-   (^ (/ "April" "april" "Apr" "apr") (list :month 4))
-   (^ (/ "May" "may") (list :month 5))
-   (^ (/ "June" "june" "Jun" "jun") (list :month 6))
-   (^ (/ "July" "july" "Jul" "jul") (list :month 7))
-   (^ (/ "August" "august" "Aug" "aug") (list :month 8))
-   (^ (/ "September" "september" "Sep" "sep") (list :month 9))
-   (^ (/ "October" "october" "Oct" "oct") (list :month 10))
-   (^ (/ "November" "november" "Nov" "nov") (list :month 11))
-   (^ (/ "December" "december" "Dec" "dec") (list :month 12))))
+(defun p/months-of-year (in)
+  (let ((token (peek-token in)))
+    (case token
+      ((January january Jan jan) (read-token in) (list :month 1))
+      ((February february Feb feb) (read-token in) (list :month 2))
+      ((March march Mar mar) (read-token in) (list :month 3))
+      ((April april Apr apr) (read-token in) (list :month 4))
+      ((May may) (read-token in) (list :month 5))
+      ((June june Jun jun) (read-token in) (list :month 6))
+      ((July july Jul jul) (read-token in) (list :month 7))
+      ((August august Aug aug) (read-token in) (list :month 8))
+      ((September september Sep sep) (read-token in) (list :month 9))
+      ((October october Oct oct) (read-token in) (list :month 10))
+      ((November november Nov nov) (read-token in) (list :month 11))
+      ((December december Dec dec) (read-token in) (list :month 12)))))
 
-(defprod p/time-unit ()
-  (/
-   (^ "nanosecond" :nanosecond)
-   (^ "microsecond" :microsecond)
-   (^ "millisecond" :millisecond)
-   (^ "second" :second)
-   (^ "minute" :minute)
-   (^ "hour" :hour)
-   (^ "day" :day)
-   (^ "week" :week)
-   (^ "month" :month)
-   (^ "year" :year)))
+(defun p/time-unit (in)
+  (let ((token (peek-token in)))
+    (case token
+      ((nanosecond nanoseconds) (read-token in) :nanosecond)
+      ((microsecond microseconds) (read-token in) :microsecond)
+      ((millisecond milliseconds) (read-token in) :millisecond)
+      ((second seconds) (read-token in) :second)
+      ((minute minutes) (read-token in) :minute)
+      ((hour hours) (read-token in) :hour)
+      ((day days) (read-token in) :day)
+      ((week weeks) (read-token in) :week)
+      ((month months) (read-token in) :month)
+      ((year years) (read-token in) :year))))
 
-(defprod p/period-unit ()
-  (/
-   (^ "per nanosecond"  (list :every (list :duration :nanosecond 1)))
-   (^ "per microsecond"  (list :every (list :duration :microsecond 1)))
-   (^ "per millisecond"  (list :every (list :duration :millisecond 1)))
-   (^ "per second"       (list :every (list :duration :second 1)))
-   (^ "per minute"       (list :every (list :duration :minute 1)))
-   (^ "hourly"           (list :every (list :duration :hour 1)))
-   (^ "daily"            (list :every (list :duration :day 1)))
-   (^ "weekly"           (list :every (list :duration :week 1)))
-   (^ "monthly"          (list :every (list :duration :month 1)))
-   (^ (/ "yearly" "annually") (list :every (list :duration :year 1)))))
+(defun p/period-unit (in)
+  (let ((token (peek-token in)))
+    (if (eq token 'per)
+	(progn
+	  (read-token in)
+	  (setf token (read-token in))
+	  (case token
+	    (nanosecond
+	     (read-token in) (list :every (list :duration :nanosecond 1)))
+	    (microsecond (read-token in)
+			 (list :every (list :duration :microsecond 1)))
+	    (millisecond (read-token in)
+			 (list :every (list :duration :millisecond 1)))
+	    (second (read-token in) (list :every (list :duration :second 1)))
+	    (minute (read-token in) (list :every (list :duration :minute 1)))
+	    ))
+	(case token
+	  (hourly (read-token in) (list :every (list :duration :hour 1)))
+	  (daily (read-token in) (list :every (list :duration :day 1)))
+	  (weekly (read-token in) (list :every (list :duration :week 1)))
+	  (monthly (read-token in) (list :every (list :duration :month 1)))
+	  ((yearly annually)
+	   (read-token in) (list :every (list :duration :year 1)))))))
 
 ;;;_ * A fixed point in time
 
-(defprod p/fixed-time (year month day)
-  (/
-   (^ "now" (list :fixed :now))
-   (^ "today" (list :rel :today))
-   (^ "tomorrow" (list :rel :tomorrow))
-   (^ "yesterday" (list :rel :yesterday))
+(defun p/fixed-time (in)
+  (let ((token (peek-token in)))
+    (case token
+      (now (read-token in) (list :fixed :now))
 
-   (^ ((^ p/number (setf year p/number)) "/"
-       (^ p/number (setf month p/number))
-       (? ("/" (^ p/number (setf day p/number)))))
-      (if day
-	  (list :fixed :year year :month month :day day)
-	  (list :fixed :year year :month month :day day)))
+      (today (read-token in) (list :rel :today))
+      (tomorrow (read-token in) (list :rel :tomorrow))
+      (yesterday (read-token in) (list :rel :yesterday))
 
-   (^ (p/months-of-year (? p/ws (/ p/ordinal p/cardinal)))
-      (let ((number (or p/ordinal p/cardinal)))
-	(if number
-	    (list :rel :this
-		  (append (list :rel) p/months-of-year
-			  (list (if (> number 40) :year :day)
-				number)))
-	    (list :rel :this
-		  (append (list :rel) p/months-of-year)))))))
+      (otherwise
+       (let ((months-of-year (p/months-of-year in)))
+	 (if months-of-year
+	     (let ((number (or (p/cardinal in)
+			       (p/ordinal in))))
+	       (list :rel :this
+		     (append (list :rel) months-of-year
+			     (if number
+				 (list (if (> number 40) :year :day)
+				       number)))))
+	     (let ((first (p/number in)))
+	       (when first
+		 (if (char= #\/ (peek-char nil in))
+		     (let ((second (p/number in)))
+		       (if (char= #\/ (peek-char nil in))
+			   (let ((third (p/number in)))
+			     ;; jww (2007-11-28): Backtrack all the way if
+			     ;; this fails!
+			     (when third
+			       (list :fixed :year first :month second
+				     :day third)))
+			   (list :fixed :year first :month second)))
+		     (progn
+		       (unread-token in first)
+		       nil))))))))))
 
 ;;;_ * A duration of time
 
-(defprod p/time-duration (duration)
-  (^ ((^ p/duration-spec (setf duration p/duration-spec))
-      (? (+
-	  ((/ ("," (? p/ws))
-	      (p/ws "and" p/ws)
-	      ("," (? p/ws) "and" p/ws))
-	   (^ p/duration-spec
-	      (nconc duration p/duration-spec))))))
-     (cons :duration duration)))
+(defun p/time-duration (in)
+  (let ((duration (p/duration-spec in)))
+    (when duration
+      ;; jww (2007-11-28): I don't support "3 months, 2 weeks" yet
+      (loop for and-next-p = (eq 'and (peek-token in))
+	 while and-next-p do (nconc duration (p/duration-spec in)))
+      (cons :duration duration))))
 
-(defprod p/duration-spec (reverse moment)
-  (/
-   (^ (p/cardinal p/ws p/time-unit (? #\s))
-      (list p/time-unit p/cardinal))
-   (^ ((? ("a" p/ws)) p/time-unit)
-      (list p/time-unit 1))))
+(defun p/duration-spec (in)
+  (let ((number (p/cardinal in)))
+    (if number
+	;; jww (2007-11-28): What if this fails?
+	(list (p/time-unit in) number)
+	(progn
+	  (if (eq 'a (peek-token in))
+	      (read-token in))
+	  ;; jww (2007-11-28): Check for error
+	  (list (p/time-unit in) 1)))))
 
 ;;;_ * A relative point in time
 
-(defprod p/relative-time ()
-  (/
-   (^ ("the" p/ws p/time-reference)
-      (list :rel :next p/time-reference))
+(defun p/relative-time (in)
+  (let ((number (p/cardinal in)))
+    (if number
+	(list number (p/time-reference in))
+	(case (peek-token in)
+	  (the (list :rel :next (p/time-reference in)))
 
-   (^ ("this" p/ws p/time-reference)
-      (list :rel :this
-	    (if (eq :duration (car p/time-reference))
-		(cons :rel (rest p/time-reference))
-		p/time-reference)))
+	  (this
+	   (let ((reference (p/time-reference in)))
+	     (list :rel :this
+		   (if (eq :duration (car reference))
+		       (cons :rel (rest reference))
+		       reference))))
 
-   (^ ((/ "next" "following") p/ws p/time-reference)
-      (list :rel :next
-	    (if (eq :duration (car p/time-reference))
-		(cons :rel (rest p/time-reference))
-		p/time-reference)))
+	  ((next following)
+	   (let ((reference (p/time-reference in)))
+	     (list :rel :next
+		   (if (eq :duration (car reference))
+		       (cons :rel (rest reference))
+		       reference))))
 
-   (^ ((/ "last" "previous" "preceeding") p/ws p/time-reference)
-      (list :rel :last
-	    (if (eq :duration (car p/time-reference))
-		(cons :rel (rest p/time-reference))
-		p/time-reference)))
+	  ((last previous preceeding)
+	   (let ((reference (p/time-reference in)))
+	     (list :rel :last
+		   (if (eq :duration (car reference))
+		       (cons :rel (rest reference))
+		       reference))))
 
-   (^ (p/cardinal p/ws p/time-reference (? #\s))
-      (list p/cardinal p/time-reference))
+;;;	  (((? the p/ws) p/ordinal p/ws p/time-reference (? #\s))
+;;;	   (list p/ordinal (if (eq :duration (car p/time-reference))
+;;;			       (cons :rel (rest p/time-reference))
+;;;			       p/time-reference)))
 
-   (^ ((? "the" p/ws) p/ordinal p/ws p/time-reference (? #\s))
-      (list p/ordinal (if (eq :duration (car p/time-reference))
-			  (cons :rel (rest p/time-reference))
-			  p/time-reference)))
+	  ((before prior)
+	   (list :rel :before (p/time-reference in)))
 
-   (^ ((/ "before" "prior to" "prior") p/ws p/time-reference)
-      (list :rel :before p/time-reference))
+	  (after
+	   (list :rel :after (p/time-reference in)))
 
-   (^ ("after" p/ws p/time-reference)
-      (list :rel :after p/time-reference))
+	  ((beginning ;; (the p/ws beginning p/ws of)
+	    )
+	   (list :rel :begin (p/time-reference in)))
 
-   (^ ((/ "beginning"
-	  ("the" p/ws "beginning" p/ws "of"))
-       p/ws p/time-reference)
-      (list :rel :begin p/time-reference))
+	  (( ;; (the p/ws beginning p/ws of)
+	    starting
+	    ;; (the p/ws start p/ws of)
+	    from
+	    since)
+	   (list :rel :from (p/time-reference in)))
 
-   (^ ((/ "beginning"
-	  ("the" p/ws "beginning" p/ws "of")
-	  "starting"
-	  ("the" p/ws "start" p/ws "of")
-	  "from"
-	  "since")
-       p/ws p/time-reference)
-      (list :rel :from p/time-reference))
-
-   (^ ((/ "in" "during")
-       p/ws p/time-reference)
-      (list :rel :in p/time-reference))
+	  ((in during)
+	   (list :rel :in (p/time-reference in)))
    
-   (^ ((/ "ending"
-	  ("the" p/ws "end" p/ws "of")
-	  "stopping"
-	  "finishing"
-	  "to"
-	  "until")
-       (p/ws p/time-reference))
-      (list :rel :to p/time-reference))
+	  ((/ ;; (the p/ws end p/ws of)
+	      stopping
+	      finishing
+	      to
+	      until)
+	   (list :rel :to (p/time-reference in)))
 
-   (^ ("of" p/ws p/time-reference)
-      (list :rel :of p/time-reference))
+	  (of
+	   (list :rel :of (p/time-reference in)))
    
-   (^ ((/ "ending"
-	  ("the" p/ws "end" p/ws "of"))
-       (p/ws p/time-reference))
-      (list :rel :end p/time-reference))))
+	  ((ending
+	    ;; (the p/ws end p/ws of)
+	    )
+	   (list :rel :end (p/time-reference in)))))))
 
-;;;_ * The high-level entry point
-
-(defprod p/time-reference ()
-  (/
-   (^ p/fixed-time     p/fixed-time)
-   (^ p/days-of-week   p/days-of-week)
-   (^ p/period-unit    p/period-unit)
-   (^ p/time-duration  p/time-duration)
-   (^ p/relative-time  p/relative-time)))
-
-(defun peek-token (in &optional (eof-error-p t)))
-(defun read-token (in &optional (eof-error-p t)))
+(defun p/time-reference (in)
+  (or (p/fixed-time in)
+      (p/days-of-week in)
+      (p/period-unit in)
+      (p/time-duration in)
+      (p/relative-time in)))
 
 (defun p/qualified-time (in)
   (let ((everyp (eq (peek-token in) 'every))
@@ -236,7 +273,7 @@
       (setf result (if everyp
 		       (list :every time)
 		       time)))
-    (loop for time = (p/time-reference in nil)
+    (loop for time = (p/time-reference in)
        while time do (setf result (list result time)))
     result))
 
@@ -247,71 +284,6 @@
     time))
 
 ;;;_ * A recurring period of time
-
-;;(defprod p/time-period ()
-;;  (/
-;;   ;; monthly [from now until when]
-;;   (^ (p/time-step (? (p/ws p/time-range)))
-;;      (time-period :range p/time-range :step p/time-step :skip nil))
-;;
-;;   ;; every 2 weeks for a year ...
-;;   (^ (p/time-step "for" p/time-duration (? (p/ws p/time-range)))
-;;      (time-period :range p/time-range :step p/time-step :skip nil))
-;;
-;;   ;; (for) 2 hours weekly ...
-;;   (^ ((? "for") p/time-duration p/time-step (? (p/ws p/time-range)))
-;;      (time-period :range p/time-range :step p/time-step :skip nil))))
-;;
-;;(defprod p/time-step ()
-;;  (/
-;;   (^ ("every" p/ws p/time-duration) p/time-duration)
-;;   (^ p/period-unit p/period-unit)))
-
-;;;_ * These are the top level entry points, which return real objects
-
-;;(defparser fixed-time-parser
-;;    (^ p/time-range
-;;       (calculate-anchor p/time-range)))
-;;
-;;(defun parse-fixed-time (string)
-;;  (multiple-value-bind (ok value) (fixed-time-parser string)
-;;    (if ok value nil)))
-;;
-;;(defparser duration-parser
-;;    (^ p/time-duration
-;;       (progn
-;;	 ;; for side effects...
-;;	 (calculate-anchor p/time-duration)
-;;	 (time-quantity-duration p/time-duration))))
-;;
-;;(defun parse-duration (string)
-;;  (multiple-value-bind (ok value) (duration-parser string)
-;;    (if ok value nil)))
-;;
-;;(defparser time-range-parser
-;;    (^ p/time-range
-;;       (progn
-;;	 ;; for side effects...
-;;	 (calculate-anchor p/time-range)
-;;	 (time-range :begin (time-quantity-anchor p/time-range)
-;;		     :end (time-quantity-terminus p/time-range)
-;;		     :duration (time-quantity-duration p/time-range)))))
-;;
-;;(defun parse-time-range (string)
-;;  (multiple-value-bind (ok value) (time-range-parser string)
-;;    (if ok value nil)))
-;;
-;;(defparser time-period-parser (^ p/time-period))
-;;
-;;(defun parse-time-period (string)
-;;  (multiple-value-bind (ok value) (time-period-parser string)
-;;    (if ok value nil)))
-
-;;(defparser time-parser (^ p/time))
-;;
-;;(defun parse-time (string)
-;;  (multiple-value-bind (ok value) (time-parser string)
-;;    (if ok value nil)))
 
 (defun compile-duration (data)
   (let (new-list)
@@ -424,11 +396,6 @@
 			(funcall function (funcall result anchor)))
 		      function))))
 	(or result #'identity))))
-
-;;(defmacro tdp (production input)
-;;  `((lambda (x)
-;;      (parselet ((foo (^ ,production)))
-;;        (foo x))) ,input))
 
 (defun time-parser-tests ()
   (dolist
